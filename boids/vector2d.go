ef1f3fc7e6ee83e3410398605618e6c46024a66c
Cassio Roos <cassio.roos@dextra-sw.com>
2020-10-12 11:33:31 -0300

    boid runnig correctly

--- boids/vector2d.go
+++ boids/vector2d.go
@@ -8,27 +8,27 @@ type Vector2D struct {
 }
 
 func (v1 Vector2D) Add(v2 Vector2D) Vector2D {
-	return Vector2D{X: v1.X + v2.X, Y: v1.Y + v2.Y}
+	return Vector2D{v1.X + v2.X, v1.Y + v2.Y}
 }
 
 func (v1 Vector2D) Subtract(v2 Vector2D) Vector2D {
-	return Vector2D{X: v1.X + v2.X, Y: v1.Y + v2.Y}
+	return Vector2D{v1.X - v2.X, v1.Y - v2.Y}
 }
 
 func (v1 Vector2D) Multiply(v2 Vector2D) Vector2D {
-	return Vector2D{X: v1.X + v2.X, Y: v1.Y + v2.Y}
+	return Vector2D{v1.X * v2.X, v1.Y * v2.Y}
 }
 
-func (v1 Vector2D) AddV(v float64) Vector2D {
-	return Vector2D{X: v1.X + v, Y: v1.Y + v}
+func (v1 Vector2D) AddV(d float64) Vector2D {
+	return Vector2D{v1.X + d, v1.Y + d}
 }
 
-func (v1 Vector2D) MultiplyV(v float64) Vector2D {
-	return Vector2D{X: v1.X * v, Y: v1.Y * v}
+func (v1 Vector2D) MultiplyV(d float64) Vector2D {
+	return Vector2D{v1.X * d, v1.Y * d}
 }
 
-func (v1 Vector2D) DivisionV(v float64) Vector2D {
-	return Vector2D{X: v1.X / v, Y: v1.Y / v}
+func (v1 Vector2D) DivisionV(d float64) Vector2D {
+	return Vector2D{v1.X / d, v1.Y / d}
 }
 
 func (v1 Vector2D) Limit(lower, upper float64) Vector2D {
