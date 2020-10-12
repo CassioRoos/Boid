aae8269beaed0793fa7ebb235eb0d41c22bb3125
Cassio Roos <cassio.roos@dextra-sw.com>
2020-10-12 20:45:55 -0300

    BOIDS COMPLETED

--- boids/boid.go
+++ boids/boid.go
@@ -12,7 +12,7 @@ import (
 const (
 	ScreenWidth, ScreenHeight = 640, 360
 	BoidCount                 = 500
-	viewRadius                = 13
+	viewRadius                = 10
 	adjRate                   = 0.015
 )
 
@@ -21,11 +21,13 @@ var (
 	red         = color.RGBA{R: 240, G: 52, B: 52, A: 255}
 	yellow      = color.RGBA{R: 255, G: 165, B: 0, A: 255}
 	blue        = color.RGBA{R: 0, G: 0, B: 255, A: 255}
-	colors      = []color.RGBA{green, red, yellow, blue}
+	white       = color.RGBA{R: 255, G: 255, B: 255, A: 1}
+	fuchsia     = color.RGBA{R: 255, G: 0, B: 255, A: 1}
+	colors      = []color.RGBA{white, green, red, yellow, blue, fuchsia}
 	Boids       [BoidCount]*Boid
 	actualColor = -1
 	boidMap     [ScreenWidth + 1][ScreenHeight + 1]int
-	lock        = sync.Mutex{}
+	rwLock      = sync.RWMutex{}
 )
 
 func init() {
@@ -45,9 +47,9 @@ type Boid struct {
 
 func (b *Boid) calcAcceleration() Vector2D {
 	upper, lower := b.Position.AddV(viewRadius), b.Position.AddV(-viewRadius)
-	avgPosition, avgVelocity := Vector2D{0, 0}, Vector2D{0, 0}
+	avgPosition, avgVelocity, separation := Vector2D{0, 0}, Vector2D{0, 0}, Vector2D{0, 0}
 	count := 0.0
-	lock.Lock()
+	rwLock.RLock()
 	for i := math.Max(lower.X, 0); i <= math.Min(upper.X, ScreenWidth); i++ {
 		for j := math.Max(lower.Y, 0); j <= math.Min(upper.Y, ScreenHeight); j++ {
 			if otherBoidId := boidMap[int(i)][int(j)]; otherBoidId != -1 && otherBoidId != b.Id {
@@ -55,36 +57,32 @@ func (b *Boid) calcAcceleration() Vector2D {
 					count++
 					avgVelocity = avgVelocity.Add(Boids[otherBoidId].Velocity)
 					avgPosition = avgPosition.Add(Boids[otherBoidId].Position)
+					separation = separation.Add(b.Position.Subtract(Boids[otherBoidId].Position).DivisionV(dist))
+
 				}
 			}
 		}
 	}
-	lock.Unlock()
-	accel := Vector2D{0, 0}
+	rwLock.RUnlock()
+	accel := Vector2D{b.borderBounce(b.Position.X, ScreenWidth), b.borderBounce(b.Position.Y, ScreenHeight)}
 	if count > 0 {
 		avgVelocity, avgPosition = avgVelocity.DivisionV(count), avgPosition.DivisionV(count)
 		accelAligment := avgVelocity.Subtract(b.Velocity).MultiplyV(adjRate)
 		accelCohesion := avgPosition.Subtract(b.Position).MultiplyV(adjRate)
-		accel = accel.Add(accelAligment).Add(accelCohesion)
+		accelSeparation := separation.MultiplyV(adjRate * 2)
+		accel = accel.Add(accelAligment).Add(accelCohesion).Add(accelSeparation)
 	}
 	return accel
 }
 
 func (b *Boid) MoveOne() {
 	calcAcceleration := b.calcAcceleration()
-	lock.Lock()
+	rwLock.Lock()
 	b.Velocity = b.Velocity.Add(calcAcceleration).Limit(-1, 1)
 	boidMap[int(b.Position.X)][int(b.Position.Y)] = -1
 	b.Position = b.Position.Add(b.Velocity)
 	boidMap[int(b.Position.X)][int(b.Position.Y)] = b.Id
-	next := b.Position.Add(b.Velocity)
-	if next.X >= ScreenWidth || next.X < 0 {
-		b.Velocity = Vector2D{-b.Velocity.X, b.Velocity.Y}
-	}
-	if next.Y >= ScreenHeight || next.Y < 0 {
-		b.Velocity = Vector2D{b.Velocity.X, -b.Velocity.Y}
-	}
-	lock.Unlock()
+	rwLock.Unlock()
 }
 
 func (b *Boid) Start() {
@@ -92,6 +90,15 @@ func (b *Boid) Start() {
 		b.MoveOne()
 		time.Sleep(5 * time.Millisecond)
 	}
+}
+
+func (b *Boid) borderBounce(pos, maxBorderPos float64) float64 {
+	if pos < viewRadius {
+		return 1 / pos
+	} else if pos > maxBorderPos-viewRadius {
+		return 1 / (pos - maxBorderPos)
+	}
+	return 0
 }
 
 func getColor() color.RGBA {
